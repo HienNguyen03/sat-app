f4db96412dae7361af7334d8aa9d5ce7b54e108c
THU HIEN <lily31392@gmail.com>
2016-10-28 00:10:24 +0300

    adc

--- src/main/java/com/se/sat/app/entity/StudentGroup.java
+++ src/main/java/com/se/sat/app/entity/StudentGroup.java
@@ -4,9 +4,12 @@ import java.util.Date;
 
 public class StudentGroup {
 	
+	// Phuc commit
 	private int id;
 	private String codeName;
 	private Date startDate;
 	private Date endDate;
 	private SuperStudentGroup superStudentGroup;
+	
+	//comment by Hien
 }
