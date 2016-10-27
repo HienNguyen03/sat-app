0122d21f07408d7c25d15ce477dce0a4adb12140
THU HIEN <lily31392@gmail.com>
2016-10-27 23:14:54 +0300

    delete message

--- src/main/java/com/se/sat/app/entity/StudentGroup.java
+++ src/main/java/com/se/sat/app/entity/StudentGroup.java
@@ -8,7 +8,5 @@ public class StudentGroup {
 	private String codeName;
 	private Date startDate;
 	private Date endDate;
-	
-	// superStudentGroup instead of superStudentGroupId
 	private SuperStudentGroup superStudentGroup;
 }
