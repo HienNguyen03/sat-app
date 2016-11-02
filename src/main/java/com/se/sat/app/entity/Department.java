4ab2e5b1c15426cb33ff86f570ad48f290d12feb
James Ho <jamesho287@gmail.com>
2016-11-02 22:24:32 +0200

    first half - entities

--- src/main/java/com/se/sat/app/entity/Department.java
+++ src/main/java/com/se/sat/app/entity/Department.java
@@ -1,12 +1,79 @@
 package com.se.sat.app.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="department")
 public class Department {
 	
+	@Id
+	@Column(name="ID")
 	private int id;
+	
+	@Column(name="CODE_NAME")
 	private String codeName;
+	
+	@Column(name="NAME")
 	private String name;
+	
+	@Column(name="DESCRIPTION")
 	private String description;
+	
+	@Column(name="LANGUAGE")
 	private String language;
+	
 	private Department superDepartment;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCodeName() {
+		return codeName;
+	}
+
+	public void setCodeName(String codeName) {
+		this.codeName = codeName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public Department getSuperDepartment() {
+		return superDepartment;
+	}
+
+	public void setSuperDepartment(Department superDepartment) {
+		this.superDepartment = superDepartment;
+	}
 	
 }
--- src/main/java/com/se/sat/app/entity/Information.java
+++ src/main/java/com/se/sat/app/entity/Information.java
@@ -2,11 +2,70 @@ package com.se.sat.app.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="information")
 public class Information {
+	
+	@Id
+	@Column(name="ID")
 	private int id;
+	
+	@Column(name="APPLIED_DATE")
 	private Date appliedDate;
+	
+	@Column(name="ACTIVATED_DURATION")
 	private int activatedDuration;
+	
+	@Column(name="ATTEMPTS")
 	private int attempts;
-	private int validRegisteredDurarion;
+	
+	@Column(name="VALID_REGISTERED_DURATION")
+	private int validRegisteredDuration;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getAppliedDate() {
+		return appliedDate;
+	}
+
+	public void setAppliedDate(Date appliedDate) {
+		this.appliedDate = appliedDate;
+	}
+
+	public int getActivatedDuration() {
+		return activatedDuration;
+	}
+
+	public void setActivatedDuration(int activatedDuration) {
+		this.activatedDuration = activatedDuration;
+	}
+
+	public int getAttempts() {
+		return attempts;
+	}
+
+	public void setAttempts(int attempts) {
+		this.attempts = attempts;
+	}
+
+	public int getValidRegisteredDuration() {
+		return validRegisteredDuration;
+	}
+
+	public void setValidRegisteredDuration(int validRegisteredDuration) {
+		this.validRegisteredDuration = validRegisteredDuration;
+	}
 	
 }
