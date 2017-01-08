345d39c360fc628a07c53c50ded54c283ecb3be2
THU HIEN <THU HIEN@THUHIEN_LAPTOP>
2017-01-08 22:07:33 +0200

    last commit 1

--- src/main/java/com/se/sat/app/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.se.sat.app;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
