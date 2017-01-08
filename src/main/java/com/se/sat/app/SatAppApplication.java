70b24735ade3555ed1a9babbcb3e6da6903df36e
THU HIEN <THU HIEN@THUHIEN_LAPTOP>
2017-01-08 20:07:15 +0200

    last project commit

--- src/main/java/com/se/sat/app/SatAppApplication.java
+++ src/main/java/com/se/sat/app/SatAppApplication.java
@@ -2,11 +2,18 @@ package com.se.sat.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SatAppApplication {
+public class SatAppApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SatAppApplication.class);
+	}
+	
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SatAppApplication.class, args);
 	}
 }
